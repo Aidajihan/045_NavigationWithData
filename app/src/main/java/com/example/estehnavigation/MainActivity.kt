8b37ef03a010e5bb98c5da518543aa1b30995a3f
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-11-16 11:50:38 +0700

    Memanggil EstehApp di MainActivity

--- app/src/main/java/com/example/estehnavigation/MainActivity.kt
+++ app/src/main/java/com/example/estehnavigation/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.estehnavigation.ui.theme.EstehNavigationTheme
+import com.example.navigasi.EsJumboAppBar
+import com.example.navigasi.EsJumbooApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +24,21 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                   EsJumbooApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     EstehNavigationTheme {
-        Greeting("Android")
+        HalamanHome {
+
+        }
     }
 }
