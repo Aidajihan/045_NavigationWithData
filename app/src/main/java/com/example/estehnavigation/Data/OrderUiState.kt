5782fb4a9841cc846713915baa49df1a41fed303
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-11-16 11:49:51 +0700

    Menambahkan OrderUiState

--- app/src/main/java/com/example/estehnavigation/Data/OrderUiState.kt
+++ app/src/main/java/com/example/estehnavigation/Data/OrderUiState.kt
@@ -4,7 +4,10 @@ data class OrderUiState(
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = "",
-    val nama : String ="",
-    val alamat: String = "",
-    val noTelp: String = "",
-)
+    val namaPelanggan: String ="",
+    val nomorTelepon: String ="",
+    val alamat: String =""
+
+) {
+
+}
